634ce58ec72165cccec501368c66a79c7e1362c1
Layne Silveira <laynepsilveira@gmail.com>
2023-10-13 20:52:37 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -2,89 +2,42 @@ import { useRef, useState } from 'react'
 import './App.css'
 
 import AudioPlayer from './AudioPlayer';
+import audioBufferToWav from 'audiobuffer-to-wav';
+import axios from 'axios';
+
+type TranscribeResp = {
+  message: string
+};
 
 function App() {
 
   const [audioSegments, setAudioSegments] = useState<AudioBuffer[]>([]);
+  const [texts, setTexts] = useState<string[]>([]);
 
+  const handleTranscribe = async () => {
+    for(const buffer of audioSegments) {
+      const wavArrayBuffer = audioBufferToWav(buffer);
+      
+      const blob = new Blob([wavArrayBuffer], { type: "audio/wav" });
 
-  // const handleAudioInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = e.target.files?.[0];
+      const formData = new FormData();
+      formData.append('file', blob);
 
-  //   if (file) {
-  //     const audioContext = new AudioContext();
+      const { data } = await axios.post<TranscribeResp>(
+        'http://localhost:8000/transcribe/',
+        formData,
+        {
+          headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3MjQyMTY4LCJpYXQiOjE2OTcyMzQ4MTMsImp0aSI6IjQ1ZGNjM2JlY2IzNzRiNDI5M2E3YTk4ZmY5YTg0NGU1IiwidXNlcl9pZCI6OX0.aMLdiOndr140h0ZumlOb3-ETCShfdT1R9kZSLkeK-38'
+          }
+        }
+      );
 
-  //     // Lê o arquivo de entrada como um ArrayBuffer
-  //     const arrayBuffer = await readFileAsArrayBuffer(file);
+      console.log(data.message);
 
-  //     // Decodifica o ArrayBuffer para um AudioBuffer
-  //     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-
-  //     // Divide o áudio em segmentos
-  //     const segmentDurationInSeconds = 5; // Defina a duração desejada para cada segmento
-  //     const sampleRate = audioBuffer.sampleRate;
-  //     const segmentLength = Math.floor(segmentDurationInSeconds * sampleRate);
-  //     const numSegments = Math.ceil(audioBuffer.length / segmentLength);
-  //     const segments: AudioBuffer[] = [];
-
-  //     for (let i = 0; i < numSegments; i++) {
-  //       const start = i * segmentLength;
-  //       const end = Math.min((i + 1) * segmentLength, audioBuffer.length);
-  //       const segmentData = audioBuffer.getChannelData(0).slice(start, end);
-  //       const segment = audioContext.createBuffer(1, segmentData.length, sampleRate);
-  //       segment.getChannelData(0).set(segmentData);
-  //       segments.push(segment);
-  //     }
-
-  //     setAudioSegments(segments);
-  //   }
-  // };
-  // const handleAudioInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = e.target.files?.[0];
-  
-  //   if (file) {
-  //     const audioContext = new AudioContext();
-  
-  //     // Lê o arquivo de entrada como um ArrayBuffer
-  //     const arrayBuffer = await readFileAsArrayBuffer(file);
-  
-  //     // Decodifica o ArrayBuffer para um AudioBuffer
-  //     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-  
-  //     // Divide o áudio em segmentos com base em pausas
-  //     const sampleRate = audioBuffer.sampleRate;
-  //     const audioData = audioBuffer.getChannelData(0);
-  //     const segments: AudioBuffer[] = [];
-  //     let segmentStart = 0;
-  
-  //     // Define um limiar de volume para considerar como "silêncio"
-  //     const silenceThreshold = 0.4; // Ajuste conforme necessário
-  
-  //     for (let i = 0; i < audioData.length; i++) {
-  //       if (Math.abs(audioData[i]) < silenceThreshold) {
-  //         // Se o volume for menor que o limiar, considere isso como uma pausa
-  //         const segmentLength = i - segmentStart;
-  //         if (segmentLength > 0) {
-  //           // Crie um segmento se a pausa for maior que zero
-  //           const segment = audioContext.createBuffer(1, segmentLength, sampleRate);
-  //           const segmentData = audioData.slice(segmentStart, i);
-  //           segment.getChannelData(0).set(segmentData);
-  //           segments.push(segment);
-  //         }
-  //         segmentStart = i + 1; // Comece o próximo segmento após a pausa
-  //       }
-  //     }
-  
-  //     if (segments.length === 0) {
-  //       // Se não houver pausas detectadas, divida o áudio em um único segmento
-  //       segments.push(audioBuffer);
-  //     }
-  
-  //     // Configure os segmentos no estado ou faça algo com eles
-  //     console.log('Número de segmentos:', segments.length);
-  //     setAudioSegments(segments);
-  //   }
-  // };
+      //setTexts(data.message)
+    }
+  }
 
   const handleAudioInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -105,12 +58,11 @@ function App() {
       let segmentStart = 0;
       let silenceStartTime = 0;
   
-      
       // Define um limiar de volume para considerar como "silêncio"
-      const silenceThreshold = 0.13; // Ajuste conforme necessário
+      const silenceThreshold = 0.1; // Ajuste conforme necessário
   
       // Define o período de tempo para considerar como uma pausa
-      const silenceDurationThreshold = 0.16; // Em segundos, ajuste conforme necessário
+      const silenceDurationThreshold = 0.35; // Em segundos, ajuste conforme necessário
   
       for (let i = 0; i < audioData.length; i++) {
         if (Math.abs(audioData[i]) < silenceThreshold) {
@@ -147,7 +99,9 @@ function App() {
       console.log('Número de segmentos:', segments.length);
       setAudioSegments(segments)
     }
+
   };
+
   const readFileAsArrayBuffer = (file: File): Promise<ArrayBuffer> => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -165,30 +119,6 @@ function App() {
     });
   };
 
-  
-  //   if (audioInputRef.current) {
-  //     const audioStream = audioInputRef.current.files?.[0];
-  //     if (audioStream) {
-  //       const mediaRecorder = new MediaRecorder(audioStream);
-  //       const segments: ArrayBuffer[] = [];
-
-  //       mediaRecorder.ondataavailable = (event) => {
-  //         if (event.data.size > 0) {
-  //           segments.push(event.data);
-  //         }
-  //       };
-
-  //       mediaRecorder.onstop = () => {
-  //         setAudioSegments(segments);
-  //       };
-
-  //       mediaRecorder.start();
-  //       setTimeout(() => {
-  //         mediaRecorder.stop();
-  //       }, 5000); // Gravação por 5 segundos (ajuste conforme necessário)
-  //     }
-  //   }
-  // };
   return (
     <>
       <div>
@@ -202,6 +132,7 @@ function App() {
         </ul> */}
       </div>
       <AudioPlayer audioSegments={audioSegments}/>
+      <button onClick={handleTranscribe}>Transcrever</button>
     </>
   )
 }
